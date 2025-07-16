d6def708707a7753848dcd3fb063842e5347d9e7
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-07-16 18:06:58 -0500

    fix: update redirect path from '/general' to '/dashboard' in Home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/router';
 export default function Home() {
    const router = useRouter();
    useEffect(() => {
-    router.replace('/general');
+    router.replace('/dashboard');
   }, [router]);
   return (
    <div></div>
